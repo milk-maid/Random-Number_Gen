92f4c586f696771f7e4e6dad81167e8dee9c81f4
milk-maid <roqbell@gmail.com>
2023-02-21 15:58:39 +0100

    TUTOR! Bello Here Ma! I'm new to scripts! I'll be done soon! Thank You MA

--- contracts/randomNumberGen.sol
+++ contracts/randomNumberGen.sol
@@ -1,34 +1,163 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/"@chainlink/contracts
+ */
 
-    event Withdrawal(uint amount, uint when);
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+contract VRFv2Consumer is VRFConsumerBaseV2, ConfirmedOwner {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    //RAFFLE DRAW IMPLEMENTATION OD THE RANDOM NUMBER
+    struct contestant {
+        uint age; 
+        string name;
+        bool isThere;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    contestant public currentWinner;
+    address[] public contestantAddress;
+    mapping (address => contestant) public Contestants;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    uint totalContestants;
+    uint maxContestant = 100;
+    uint randomWordsGen;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
 
-        owner.transfer(address(this).balance);
+    // ABOVE ARE THE STATE VARIABLE FOR THE RAFFLE DRAW IMPLEMENTATION
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus)
+        public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/#configurations
+    bytes32 keyHash =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords = 4;
+
+    /**
+     * HARDCODED FOR GOERLI
+     * COORDINATOR: 	0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
+     */
+    constructor(
+        uint64 subscriptionId
+    )
+        VRFConsumerBaseV2(	0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D)
+        ConfirmedOwner(msg.sender)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(
+            	0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
+        );
+        s_subscriptionId = subscriptionId;
+    }
+
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords()
+        public
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        randomWordsGen = _randomWords[0];
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
+    //RAFFLE DRAW IMPLEMENTATION OD THE RANDOM NUMBER
+
+    function addContestant(uint _age, string calldata _name, address _contestantAddr) public onlyOwner returns(bool) {
+        require(_age <= 40, "Only Youths (i.e age not above 40) are allowed in");
+        require(_contestantAddr != address(0), "INPUT A VALID ADDRESS");
+        require(!Contestants[_contestantAddr].isThere, "Only Unique Contestants Must Be Unique");  
+        require(totalContestants <= maxContestant, "All Space Taken");   
+        
+        contestant memory oneContestant = contestant(_age, _name, true); 
+        Contestants[_contestantAddr] = oneContestant;
+        contestantAddress.push(_contestantAddr);
+        totalContestants = totalContestants + 1;
+        return Contestants[_contestantAddr].isThere;
+    }
+
+    function winnerGenerator() public onlyOwner returns(address winner) {
+        requestRandomWords();
+        uint randValue = randomWordsGen % totalContestants;
+        winner = contestantAddress[randValue];
+        currentWinner = Contestants[winner];
+    }
+
+    function getWinnerDetails() public view returns(contestant memory) {
+        return currentWinner;
     }
 }
